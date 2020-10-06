7cec1bffd08a12a4285a5a615c07316c3ff1e749
alexis villaraza <alexis.borja@gmail.com>
2020-10-06 14:59:04 -0700

    created Header with nav links

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <div>
+      <h1>Alexis Villaraza</h1>
+      <h2>Full Stack Web Developer</h2>
+
+      <nav>
+        <p>About Me</p>
+        <p>Resume</p>
+        <p>Portfolio</p>
+        <p>Contact Me</p>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
