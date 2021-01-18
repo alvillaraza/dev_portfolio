85151a6c6545bf1e01161d8bcfeb9ac3ad28dc94
alexis villaraza <alexis.borja@gmail.com>
2021-01-18 11:03:39 -0700

    Updates

---
+++ precache-manifest.3b6ff8231bedbea0937e62fbecfb5635.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b996394f6a9f049f395665b5a51aa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7de6e25735a6b46bf42",
+    "url": "/static/css/main.153da4b2.chunk.css"
+  },
+  {
+    "revision": "0c950075f5c080ab0008",
+    "url": "/static/js/2.709c42a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.709c42a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7de6e25735a6b46bf42",
+    "url": "/static/js/main.b24789b8.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7b1d3a06791016d41c23c73dd03e441e",
+    "url": "/static/media/acnh.7b1d3a06.png"
+  },
+  {
+    "revision": "3927577f21aabd4bc2972042b4c0a312",
+    "url": "/static/media/portfolio-resume-coffee-stain.3927577f.jpg"
+  },
+  {
+    "revision": "66dcc1d7328f9a127cb1d243de6bfcc8",
+    "url": "/static/media/prof.66dcc1d7.jpeg"
+  },
+  {
+    "revision": "4cd8c823be5ad32a7649af7d7b20b0e5",
+    "url": "/static/media/refresh.4cd8c823.png"
+  },
+  {
+    "revision": "5fa3d8b312a1a321f6a200da0376e141",
+    "url": "/static/media/ui.5fa3d8b3.png"
+  }
+]);
