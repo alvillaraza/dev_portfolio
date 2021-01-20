14c899cb79f4b3e6c2a09bb3ce637e9cf168c820
alexis villaraza <alexis.borja@gmail.com>
2021-01-19 18:14:52 -0700

    spacing

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -7,7 +7,6 @@ function AboutMe() {
         <div className='about-container'>
             <h1>About Me</h1>
             <img src={prof} alt='Yours truly!' className='my-photo' />
-
               <p>Front end: HTML | CSS | Javascript | React | Redux</p>
             <p>Back end: Node | SQL | Python</p>
             <p>
